c5037cbcc7f740e98dc24dc690e15038790738d6
Harsha5444 <harshavardhan.r@choice-solutions.com>
2025-02-11 17:37:19 +0530

    dashboard complete

--- AVUserRoleOrg.Web/Controllers/HomeController.cs
+++ AVUserRoleOrg.Web/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using AVUserRoleOrg.BLL;
+using AVUserRoleOrg.Web.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace AVUserRoleOrg.Web.Controllers
@@ -6,6 +10,7 @@ namespace AVUserRoleOrg.Web.Controllers
     public class HomeController : Controller
     {
         private readonly UserRoleOrgBLL _userRoleOrgBLL;
+        UserRoleOrgDBEntities dBEntities = new UserRoleOrgDBEntities();
         public HomeController()
         {
             _userRoleOrgBLL = new UserRoleOrgBLL();
@@ -23,6 +28,34 @@ namespace AVUserRoleOrg.Web.Controllers
         {
             var roles = _userRoleOrgBLL.GetRoles();
             return View(roles);
+        }
+        public ActionResult Dashboard()
+        {
+            //var totalUsers = dBEntities.Dashboard_Vw.Count();
+            //var activeUsers = dBEntities.Dashboard_Vw.Count(u => u.isactive == true);
+
+            //var orgWiseUsers = dBEntities.Dashboard_Vw
+            //                        .GroupBy(u => u.Orgname)
+            //                        .Select(g => new OrgWiseUser { OrgName = g.Key, Count = g.Count() })
+            //                        .ToList();
+
+            //var roleWiseUsers = dBEntities.Dashboard_Vw
+            //                        .GroupBy(u => u.rolename)
+            //                        .Select(g => new RoleWiseUser { Rolename = g.Key, Count = g.Count() })
+            //                        .ToList();
+            //var dashboardData = new DashboardViewModel
+            //{
+            //    TotalUsers = totalUsers,
+            //    ActiveUsers = activeUsers,
+            //    OrgWiseUsers = orgWiseUsers,
+            //    RoleWiseUsers = roleWiseUsers
+            //};
+            //var dashboardData2 = dBEntities.Database.SqlQuery<DashboardViewModel>("EXEC GetDashboardData").FirstOrDefault();
+            var result = dBEntities.Database.SqlQuery<DashboardViewModel>("EXEC GetDashboardData").FirstOrDefault();
+            result.OrgWiseUsers = JsonConvert.DeserializeObject<List<OrgWiseUser>>(result.OrgWiseUsersJson);
+            result.RoleWiseUsers = JsonConvert.DeserializeObject<List<RoleWiseUser>>(result.RoleWiseUsersJson);
+
+            return View(result); 
         }
     }
 }
---
+++ AVUserRoleOrg.Web/Models/DashboardViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AVUserRoleOrg.Web.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int ActiveUsers { get; set; }
+        public string OrgWiseUsersJson { get; set; }
+        public string RoleWiseUsersJson { get; set; }
+        public List<OrgWiseUser> OrgWiseUsers { get; set; } = new List<OrgWiseUser>();
+        public List<RoleWiseUser> RoleWiseUsers { get; set; } = new List<RoleWiseUser>();
+    }
+
+    public class OrgWiseUser
+    {
+        public string OrgName { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class RoleWiseUser
+    {
+        public string Rolename { get; set; }
+        public int Count { get; set; }
+    }
+}
